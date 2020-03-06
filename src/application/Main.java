f84b750b05de2b073bca7991d533aed7ca1f286a
delarasham <56743249+delarasham@users.noreply.github.com>
2020-03-05 20:42:56 -0700

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -14,26 +16,24 @@ public class Main extends Application {
 	Stage window;
 	Scene level1,level2,level3,mainMenu;
 	@Override
-	public void start(Stage primaryStage) {
-		// this is sample main just to demonstrate how the puzzle window will work
-		// once the levels are created we can set handlers to display the puzzle window once a puzzle is clicked
-		window = primaryStage;
-		Puzzle p = new Puzzle(0,0,"question","answer");
-		Button c = new Button("click to change");
-		c.setOnAction(e->{
+	public void start(Stage primaryStage) throws IOException {
+		Pane root = new Pane();
+		Button b = new Button();
+		root.getChildren().add(b);
+		b.setOnAction(e->{
+			PuzzleWindow p = new PuzzleWindow();
+			Puzzle puzzle = new Puzzle(0,0,"question","answer");
 			try {
-				boolean ans = PuzzleWindow.display("name",p);
+				p.display("Puzzle", puzzle);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		});
-		Pane root = new Pane();
-		root.getChildren().add(c);
-		mainMenu = new Scene(root,800,600);
-		window.setScene(mainMenu);
-	    window.setTitle("Virtual Scape Room");
-	    window.show();	
+		
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root,567, 437));
+        primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
