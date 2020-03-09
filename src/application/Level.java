c88625e87b2c0caa889d70d31a19345dc0bfdc31
delarasham <56743249+delarasham@users.noreply.github.com>
2020-03-09 11:20:18 -0600

    Update Level.java

--- src/application/Level.java
+++ src/application/Level.java
@@ -86,11 +86,12 @@ public class Level {
 		ArrayList<Puzzle> puzList = new ArrayList<Puzzle>();
 		switch(lvlNum) {
 		case 1:
-			Puzzle puzzle1_1 = new Puzzle(0, 0, "Scarlett has 3 children with her husband Brad Pitt, who passed away only a year ago in a horrible plane accident. \r\n" + 
-					"He was on a business trip and extreme weather conditions overthrew the plane's course as it crashed into a forested area\r\n" + 
-					"and burned. Her children, Huey, Dewey and Louis are all married and living in the city, but they would visit Scarlett in\r\n" + 
-					"the country from time to time for her lavish parties and special occasions or holidays. The night of Scarlett's death, \r\n" + 
-					"she had thrown an extravagant party which was the perfect occasion for her children to be visiting. \r\n" + 
+			Puzzle puzzle1_1 = new Puzzle(0, 0, "Scarlett has 3 children with her husband Brad Pitt, who passed \r\n" + 
+					"away only a year ago in a horrible plane accident.He was on a business trip and extreme weather \r\n" + 
+					"conditions overthrew the plane's course as it crashed into a forested area and burned. Her children, Huey, \r\n" + 
+					"Dewey and Louis are all married and living in the city, but they would visit Scarlett in the country \r\n" + 
+					"from time to time for her lavish parties and special occasions or holidays. The night of Scarlett's death, \r\n" +
+					"she had thrown an extravagant party which was the perfect occasion for her children to be visiting. \r\n"+
 					"\r\n" + 
 					"Wait \r\n" + 
 					"\r\n" + 
@@ -101,8 +102,9 @@ public class Level {
 					"Unscramble the words and enter the answer below:", 
 					
 					"New Years Eve");
-			Puzzle puzzle1_2 = new Puzzle(0,0,"You kept looking through the desk and found a drawer with a safe in it. Shoot! It's locked. If only the code was written \r\n" + 
-					"somewhere. a ha! It is. Maybe? This is a 4 number code but you see a note with 8 numbers on it…. Maybe this is some kind \r\n" + 
+			Puzzle puzzle1_2 = new Puzzle(0,0,"You kept looking through the desk and found a drawer with a safe in it. Shoot! \r\n" + 
+					"It's locked. If only the code was written somewhere. a ha! It is.  \r\n"+
+					"Maybe? This is a 4 number code but you see a note with 8 numbers on it…. Maybe this is some kind \r\n" + 
 					"of a key. \r\n" + 
 					"\r\n" + 
 					"2  1  3  2  7  1  4  2 ", 
@@ -116,9 +118,9 @@ public class Level {
 			Puzzle puzzle2_1 = new Puzzle(0,0, "You find a file cabinet that is locked with a pass code. On it is inscribed:\r\n" + 
 					"If Two's Company, and Three's a Crowd What are Four AND Five?\r\n" + 
 					"Solve the riddle to open the file cabinet.", "9");
-			Puzzle puzzle2_2 = new Puzzle(0,0, "One door remains, and it leads to the room Scarlett died in. The murder weapon or other vital evidence\r\n" + 
-					"that would pinpoint the suspect could be behind that door. But the door is locked with what appears \r\n" + 
-					"to be a tiny computer puzzle. It reads the following:\r\n" + 
+			Puzzle puzzle2_2 = new Puzzle(0,0, "One door remains, and it leads to the room Scarlett died in. \r\n" + 
+					"The murder weapon or other vital evidence that would pinpoint the suspect could be behind that door.\r\n" + 
+					" But the door is locked with what appears to be a tiny computer puzzle. It reads the following:\r\n" + 
 					"\r\n" + 
 					"\r\n" + 
 					"1 0 1 0   1 1 0 0   1 1 1 0\r\n" + 
