00d9c4c24b2f4014057c05913833536ce035449c
delarasham <56743249+delarasham@users.noreply.github.com>
2020-03-11 19:46:09 -0600

    Update MainMenuController.java

--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -1,5 +1,9 @@
 package application;
 
+/**
+ * This is a class that handles the main menu button actions
+ * @author Delara, Paul, Carlo
+ */
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -10,15 +14,32 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainMenuController {
+	/**
+	 * This method handles the exit button click action
+	 * If the user clicks this button it will end the game by closing the main window
+	 * @param e this is an even of type ActionEvent that will be used to close the scene created in main class
+	 */
 	public void exit(ActionEvent e) {
 		Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
 		window.close();
 	}
-	public void avatarClick(ActionEvent e) {
+	
+	/**
+	 * This method handles the button for the avatar demo
+	 * If the button is clicked a new window will open that had the avatar in it
+	 * @param e 
+	 */
+	public void avatarClick() {
 		CharacterMovement movement = new CharacterMovement();
 	    movement.movement();
 
 	}
+	/**
+	 * This methos handles the start button on main menu
+	 * If the button is clicked a new scene will load from the fxml file called Level1.fxml for level 1 scene
+	 * @param e this is an even of type ActionEvent that will be used to change the scene created in main class to level 1 scene
+	 * @throws IOException on loading the fxml file
+	 */
 	public void startClick(ActionEvent e) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/Level1.fxml"));
 		Scene gameover=new Scene(root,600,400);
