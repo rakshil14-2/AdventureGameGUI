5716d5209b0372e7dfbbe2c73563ac46a0aa74e9
clairepopadynetz <60753017+clairepopadynetz@users.noreply.github.com>
2020-03-26 20:29:48 -0600

    updated Puzzle.java documentation

--- src/model/Puzzle.java
+++ src/model/Puzzle.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 public class Puzzle {
 	
 	/**
-	 * 		Puzzle Class
-	 * 		Players solve puzzles and by solving puzzles
-	 * 		are able to unlock the level door to move on to the next level.
+	 * The Puzzle Class
+	 * Players solve puzzles to play the game, and by solving puzzles are able to 
+	 * unlock the level door to move on to the next level. Every puzzle in a level has to be solved 
+	 * before the door can be unlocked. This class gets the questions and answers for the puzzles 
+	 * and checks that they are solved.
 	 */
 	
 	
-	/**
-	 * 	Puzzle attributes including the question and answer strings,
-	 * 	boolean solved, and x,y coordinates for when we do collisions and
-	 *  hit detection.
-	 */
+	//boolean solved is to check if a puzzle is solved
 	private boolean solved;
+	//x and y position of the puzzle for its map location
 	private int xPos;
 	private int yPos;
+	//puzzle question and answer strings
 	private String question, answer;
 	
+	
 	/**
-	 * Puzzle copy constructor
-	 * @param p Puzzle to be copied
+	 * This is the Puzzle copy constructor.
+	 * @param p is a Puzzle to be copied.
 	 */
 	public Puzzle(Puzzle p) {
 		this.answer = p.answer ;
@@ -33,11 +34,11 @@ public class Puzzle {
 	}
 	
 	/**
-	 * Puzzle class constructor
-	 * @param _x int for puzzles x value
-	 * @param _y int for puzzles y value
-	 * @param q String for puzzle's question
-	 * @param a String for puzzle's answer
+	 *Puzzle class constructor
+	 * @param _x is an int for a puzzle's x value.
+	 * @param _y is an int for a puzzle's y value.
+	 * @param q is a String for a puzzle's question.
+	 * @param a is a String for a puzzle's answer.
 	 */
 	public Puzzle(int _x,int _y,String q,String a) {
 		this.xPos = _x;
@@ -49,16 +50,16 @@ public class Puzzle {
 	
 	
 	/**
-	 *  isSolved is a getter to check if puzzle is solved
-	 * @return boolean true if puzzle is solved
+	 * isSolved is a getter method which to checks if a puzzle is solved.
+	 * @return boolean solved (true if puzzle is solved).
 	 */
 	public boolean isSolved() {
 		return solved;
 	}
 	
 	/**
-	 * getX getter for puzzles x position on map (not currently implemented)
-	 * @return integer of puzzle's x position (will change to double for GUI)
+	 * The getX method is a getter for a puzzle's x position on the level map.
+	 * @return integer of the puzzle's x position.
 	 */
 	public int getX()
 	{
@@ -66,15 +67,16 @@ public class Puzzle {
 	}	
 	
 	/**
-	 *  getY getter for puzzles y position on map
-	 * @return integer of puzzle's y position (will change to double for GUI)
+	 * The getY method is a getter for a puzzle's y position on the level map.
+	 * @return integer of the puzzle's y position.
 	 */
 	public int getY() {
 		return yPos;
 	}
 	
 	/**
-	 * getQuestion getter for puzzle question
+	 * The getQuestion method is a getter for a puzzle question.
+	 * The question for each puzzle has a correct answer.
 	 * @return String of puzzles question
 	 */
 	public String getQuestion() {
@@ -82,7 +84,11 @@ public class Puzzle {
 	}
 	
 	/**
-	 * getAnswer is a getter for puzzle answer
+	 * The getAnswer method is a getter for a puzzle answer.
+	 * The answer matches with a given puzzle question.
+	 * The answer the user inputs has to match tghis answer for a puzzle in order to solve
+	 * the puzzle and advance in the game.
+	 *
 	 * @return string of puzzle's answer
 	 */
 	public String getAnswer() {
@@ -91,11 +97,9 @@ public class Puzzle {
 	
 	
 	/**
-	 *  setSolved is a setter to set the puzzle as solved
+	 * The setSolved method is a setter to set the puzzle as solved.
 	 */
 	public void setSolved() {
 		solved = true;
-	}
-	
-	
+	}	
 }
