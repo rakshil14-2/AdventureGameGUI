9381acacab970808330adf14796c03e743328b7e
clairepopadynetz <60753017+clairepopadynetz@users.noreply.github.com>
2020-03-26 18:08:45 -0600

    Update GameOverController
    small changes to JavaDoc comments

--- src/controller/GameOverController.java
+++ src/controller/GameOverController.java
@@ -1,7 +1,7 @@
 package controller;
 
+//importing libraries 
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -10,15 +10,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * This class handles the events in the game over scene
+ * The GameOverController class handles the events in the game over scene
+ * the game over scene appears when the game is finished 
  * @author Delara
  *
  */
 public class GameOverController {
 	/**
-	 * This method handles the button click in game over scene
+	 * GameOverController method handles a button click in game over scene
 	 * If the button is clicked the scene will change to the main menu scene
-	 * @param e event of type ActionEvent use to change the current scene to the main menu scene
+	 * @param e is an event of type ActionEvent 
+	 * it is used to change the current scene to the main menu scene
 	 * @throws IOException exception handling for loading the MainMenu.fxml file
 	 */
 	public void buttonClick(ActionEvent e) throws IOException {
