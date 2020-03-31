f039d95f4f376eb27dae6d05199f04b429fc02b3
carlo-deg <60478683+carlo-deg@users.noreply.github.com>
2020-03-30 18:23:47 -0600

    Update Prop.java
    Revised the comments.

--- src/model/Prop.java
+++ src/model/Prop.java
@@ -1,29 +1,26 @@
 package model;
 
+
+/*
+ * The Prop class creates objects on the map for the player to interact with.
+ * These prop objects contain important storyline information which will help better the user's 
+ * understanding of the storyline and provide them with important hints and information to win the game. The intention of the props is to add more atmosphere to the game while remaining purposeful.
+ */
 public class Prop extends GameObject{//Prop class inherited from GameObject class
-	
-	/**
-	 *
-	 * The Prop class creates objects on the map for the player to interact with.
-	 * These prop objects contain important storyline information which will help better the 
-	 * user's undertsanding of the storyline and provide them with important hints and information to 
-	 * win the game. The intention of the props is to add more atmosphere to the game while remaining purposeful. 
-	 *
-	 */
 
 	/**
 	 * This is the copy contructor for Prop.
 	 *
 	 * @param prop Prop object to be copied
 	 */
-	public Prop(Prop prop)
-	{
+	public Prop(Prop prop) {
 		super(prop);
 	}
 	
 	/**
 	 * The Prop object constructor
 	 * To help place the prop in the respective location and give it the relative decription.
+	 * 
 	 * @param des prop description text
 	 * @param x integer representing x position
 	 * @param y integer representing y position
@@ -31,13 +28,4 @@ public class Prop extends GameObject{//Prop class inherited from GameObject clas
 	public Prop(String des, int x, int y) {
 		super(x,y,des);
 	} 
-	
-	
-	/**
-	 * The getDescription method is a getter method which retrieves the prop description string.
-	 * The prop requires a description for the user to read so that it maintains a purpose.
-	 *
-	 * @return string representing the description of the Prop object
-	 */
-
 }
